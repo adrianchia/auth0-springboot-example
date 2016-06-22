906dff31d01375b17f13c677f682e45176b1e513
Adrian Chia <achia@adrianchia.com>
2016-06-22 02:56:33 -0500

    Update example based on Auth0 Spring API

--- src/main/java/org/adrianchia/Application.java
+++ src/main/java/org/adrianchia/Application.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by adrianchia on 5/20/15.
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.adrianchia", "com.auth0"})
+@ComponentScan(basePackages = {"org.adrianchia", "com.auth0.spring.security.api"})
 @EnableAutoConfiguration
-@ImportResource("classpath:auth0-security-context.xml")
 @PropertySource("classpath:auth0.properties")
 public class Application {
 
--- src/main/java/org/adrianchia/SecurityConfig.java
+++ src/main/java/org/adrianchia/SecurityConfig.java
@@ -1,20 +1,29 @@
 package org.adrianchia;
 
+import com.auth0.spring.security.api.Auth0SecurityConfig;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * Created by adrianchia on 5/20/15.
  */
-@EnableWebMvcSecurity
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends Auth0SecurityConfig {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/secured/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .antMatchers(securedRoute).authenticated();
         //super.configure(http);
     }
+
 }
